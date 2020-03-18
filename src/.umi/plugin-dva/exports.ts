c88b111a33dc77c64b00926b5a24ca318f799e1c
Rajesh <rkumar1904@gmail.com>
2020-03-18 21:20:27 +0530

    updated .umi auto generated with gitignore

--- src/.umi/plugin-dva/exports.ts
+++
@@ -1,3 +0,0 @@
-
-export { connect, useDispatch, useStore, useSelector } from '/Users/rajeshpro/Development/Ant-React/ant-react-template/node_modules/dva';
-export { getApp as getDvaApp } from './dva';
